bb1609df179a4d9697ca0168e8d5250e9f070b13
postboy <anotherdiskmag@gmail.com>
2015-08-05 01:52:05 +0300

    initial efforts for SSE support in future

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #just a compilation
 
-gcc -Wall main.c scrypt/readpass.c scrypt/crypto_scrypt-ref.c scrypt/sha256.c -o cpwd
+gcc -Wall -march=native main.c scrypt/readpass.c scrypt/crypto_scrypt-ref.c scrypt/sha256.c -o cpwd
+#grep sse /proc/cpuinfo
+#gcc -Wall -march=native main.c scrypt/readpass.c scrypt/crypto_scrypt-sse.c scrypt/sha256.c -o cpwd
